84a626903586ab241ca8275b3320776d3d1ccba7
pepperonion <pepperonion@localhost.localdomain>
2021-08-29 20:15:40 +0200

    Commit5

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -19,13 +19,23 @@ var store = [{
         "tags": ["nmap,","apunts"],
         "url": "http://localhost:4000/nmap-commands/",
         "teaser":"http://localhost:4000/assets/images/htb-writeup-delivery/delivery_logo.png"},{
-        "title": "Bandit from 0 to 3",
+        "title": "Bandit from level 0 to level 3",
         "excerpt":"Level 0 to Level 1 THE LEVEL’S STATEMENT The password for the next level is stored in a file called readme located in the home directory. Use this password to log into bandit1 using SSH. Whenever you find a password for a level, use SSH (on port 2220) to log...","categories": ["OverTheWire"],
         "tags": ["bandit"],
         "url": "http://localhost:4000/bandit-0-to-3/",
         "teaser":"http://localhost:4000/assets/images/htb-writeup-delivery/delivery_logo.png"},{
-        "title": "Bandit from 3 to 6",
-        "excerpt":"Level 3 to Level 4 THE LEVEL’S STATEMENT The password for the next level is stored in a hidden file in the inhere directory. COMMANDS &gt; whatis ls ls (1) - list directory contents &gt; whatis cd cd (1) - change working directory &gt; whatis cat cat (1) - concatenate...","categories": ["OverTheWire"],
+        "title": "Bandit from level 3 to level 6",
+        "excerpt":"Level 3 to Level 4 THE LEVEL’S STATEMENT The password for the next level is stored in a hidden file in the inhere directory. COMMANDS ls - list directory contents. cd - change working directory. cat - concatenate files and print on the standard output. file - determine file type....","categories": ["OverTheWire"],
         "tags": ["bandit"],
         "url": "http://localhost:4000/bandit-3-to-6/",
+        "teaser":"http://localhost:4000/assets/images/htb-writeup-delivery/delivery_logo.png"},{
+        "title": "Bandit from 3 to 6",
+        "excerpt":"## Level 3 to Level 4 ### THE LEVEL'S STATEMENT >The password for the next level is stored in a hidden file in the inhere directory. ### COMMANDS > *ls - list directory contents.* > *cd - change working directory.* > > *cat - concatenate files and print on the...","categories": ["OverTheWire"],
+        "tags": ["bandit"],
+        "url": "http://localhost:4000/bandit-3-to-6.md/",
+        "teaser":"http://localhost:4000/assets/images/htb-writeup-delivery/delivery_logo.png"},{
+        "title": "Bandit from level 6 to level 9",
+        "excerpt":"Level 6 to Level 7 THE LEVEL’S STATEMENT The password for the next level is stored somewhere on the server and has all of the following properties: - owned by user bandit7 - owned by group bandit6 - 33 bytes in size COMMANDS ls - list directory contents. cd -...","categories": ["OverTheWire"],
+        "tags": ["bandit"],
+        "url": "http://localhost:4000/bandit-6-to-9/",
         "teaser":"http://localhost:4000/assets/images/htb-writeup-delivery/delivery_logo.png"}]
