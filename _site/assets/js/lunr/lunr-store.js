39b014d1e5f95831a1203b7b11636c4b78391633
pepperonion <pepperonion@localhost.localdomain>
2021-09-29 12:28:48 +0200

    Actualizando web6

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -29,11 +29,6 @@ var store = [{
         "tags": ["bandit"],
         "url": "http://localhost:4000/bandit-3-to-6/",
         "teaser":"http://localhost:4000/assets/images/htb-writeup-delivery/delivery_logo.png"},{
-        "title": "Bandit from 3 to 6",
-        "excerpt":"## Level 3 to Level 4 ### THE LEVEL'S STATEMENT >The password for the next level is stored in a hidden file in the inhere directory. ### COMMANDS > *ls - list directory contents.* > *cd - change working directory.* > > *cat - concatenate files and print on the...","categories": ["OverTheWire"],
-        "tags": ["bandit"],
-        "url": "http://localhost:4000/bandit-3-to-6.md/",
-        "teaser":"http://localhost:4000/assets/images/htb-writeup-delivery/delivery_logo.png"},{
         "title": "Bandit from level 6 to level 9",
         "excerpt":"Level 6 to Level 7 THE LEVEL’S STATEMENT The password for the next level is stored somewhere on the server and has all of the following properties: - owned by user bandit7 - owned by group bandit6 - 33 bytes in size COMMANDS ls - list directory contents. cd -...","categories": ["OverTheWire"],
         "tags": ["bandit"],
